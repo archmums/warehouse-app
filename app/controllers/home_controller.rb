f30a16d752e9ae81af79c8fb03046489e7821244
Lucas <loukasend@gmail.com>
2022-04-30 20:27:04 -0300

    segunda alteração

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @warehouses = Warehouse.all
   end
 end
--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -12,4 +12,33 @@ describe 'Usuario visita tela inicial' do
     #Assert
     expect(page).to have_content('Galpões & Estoque')
   end
+  it 'e vê os galpões cadastros' do
+    #Arrange
+    Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000 )
+    Warehouse.create(name: 'Maceio', code: 'MCZ', city: 'Maceio', area: 50_000 )
+
+    #Act
+    visit('/')
+    #Assert
+    expect(page).not_to have_content('Não existem galpões cadastrados')
+    expect(page).to have_content('Rio')
+    expect(page).to have_content('Código: SDU')
+    expect(page).to have_content('Cidade: Rio de Janeiro')
+    expect(page).to have_content('60000 m2')
+
+    expect(page).to have_content('Maceio')
+    expect(page).to have_content('Código: MCZ')
+    expect(page).to have_content('Cidade: Maceio')
+    expect(page).to have_content('50000 m2')
+  end
+
+  it 'e não existem galpões cadastrados' do
+    #Arrange
+
+    #Act
+    visit('/')
+
+    #Assert
+    expect(page).to have_content('Não existem galpões cadastrados')
+  end
 end
